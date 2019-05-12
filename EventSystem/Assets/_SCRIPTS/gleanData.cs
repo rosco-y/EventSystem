6d9d281fc8f9b23b9b4d1954a54c866ac51c4866
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-05-11 22:34:26 -0400

    Finally responding to an EndEdit() event inf TMP_InputField

---
+++ EventSystem/Assets/_SCRIPTS/gleanData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class gleanData : MonoBehaviour, ISubmitHandler
+
+{
+    public TMP_InputField _tmpNameField;
+
+    string _userName;
+
+    public void GetName()
+    {
+        _userName = _tmpNameField.text;
+
+        print($"This coming along most wonderfully, {_userName}.\n You are truly becoming more interactive in UNITY!!");
+    }
+
+    public void OnSubmit(BaseEventData eventData) => throw new System.NotImplementedException();
+}
